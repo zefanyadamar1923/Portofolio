4f1bd0da1d7ab0298068faf8810c0248e7d13954
zefanyadamar1923 <zefanyadamar@gmail.com>
2026-01-02 09:36:09 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* Analytics diletakkan di luar App namun tetap di dalam Provider StrictMode */}
+    <Analytics />
   </React.StrictMode>
 );
